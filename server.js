36226c99a918b4eedc54464ecc4f313b0e7743d1
VanshTheJudged <vanshk970@gmail.com>
2025-08-05 04:18:22 +0530

    v5

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use('/api/report', uploadRoutes);
 // Serve static frontend AFTER routes
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
@@ -30,6 +30,8 @@ app.get('*', (req, res) => {
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(5000, () => console.log('Server running on port 5000'));
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
   })
   .catch(err => console.log(err));
