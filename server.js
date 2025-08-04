82cbc592ec5d602189841baa22cde669b62a9f85
VanshTheJudged <vanshk970@gmail.com>
2025-08-05 04:14:50 +0530

    v4

--- server.js
+++ server.js
@@ -5,36 +5,31 @@ const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const uploadRoutes = require('./routes/upload');
 const path = require('path');
+const helmet = require('helmet');
 
 dotenv.config();
 const app = express();
 
-// Serve static files from public or frontend folder
-app.use(express.static(path.join(__dirname, 'public'))); // or 'frontend'
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html')); // or the main HTML file
-});
-
-const helmet = require('helmet');
-
-// Relax CSP a bit
-app.use(
-  helmet.contentSecurityPolicy({
-    useDefaults: true,
-    directives: {
-      "script-src": ["'self'", "'unsafe-inline'"], // Allow inline scripts
-      "default-src": ["'self'"]
-    }
-  })
-);
+// Middleware
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
+
+// Routes FIRST
 app.use('/api/auth', authRoutes);
 app.use('/api/report', uploadRoutes);
 
+// Serve static frontend AFTER routes
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'home.html'));
+});
+
+// Mongo connection
 mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-  console.log('MongoDB connected');
-  app.listen(5000, () => console.log('Server running on port 5000'));
-}).catch((err) => console.log(err));
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(5000, () => console.log('Server running on port 5000'));
+  })
+  .catch(err => console.log(err));
